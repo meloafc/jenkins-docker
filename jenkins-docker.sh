7c4feaa9f20c1bfc45e676166e2a595f50ab74ef
Alex Fabiano <afabiano15@gmail.com>
2019-06-22 16:16:04 -0400

    updating jenkins-docker.sh

--- jenkins-docker.sh
+++ jenkins-docker.sh
@@ -2,9 +2,11 @@
 sudo docker run --name jenkins-docker \
     -p 30100:8080 \
     -p 50000:50000 \
-    -v jenkins_home:/var/jenkins_home \
+    -v /home/jenkins:/var/jenkins_home \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -e JAVA_OPTS=-Duser.timezone=America/Manaus \
     --restart=unless-stopped \
-    -d meloafc/jenkins-docker
+    -d -u root meloafc/jenkins-docker
 
 # Unlock Jenkins:
 # docker exec <container> cat /var/jenkins_home/secrets/initialAdminPassword
